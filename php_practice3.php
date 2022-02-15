bed980fdbc81f128c3581be5728992bd91cbe0f3
maki <yayu.060842@gmail.com>
2022-02-15 07:01:22 +0000

    提出課題　関数2

---
+++ php_practice3.php
@@ -0,0 +1,22 @@
+<?php
+function prodct($arr){
+    $result = 0;
+    for ($i = 0; $i < count($arr); $i++){
+        if(4i == 0){
+            $result += $arr[$i];
+        }else{
+            $result *= $arr[$i];
+        }
+    }
+    return $result;
+}
+
+function product($arr){
+    $result = $arr[0];
+    for ($i = 1; $i < count($arr); $i++){
+        $result *= $arr[$i];
+    }
+    return $result;
+}
+echo product(array(1,3,5,7,9));
+?>
